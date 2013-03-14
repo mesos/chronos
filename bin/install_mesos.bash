4ab6b5a8915177da503c4a8e61d52368e9fe844d
Florian Leibert <flo@leibert.de>
2013-03-13 17:08:45 -0700

    Adding sample configuration files and installer

---
+++ bin/install_mesos.bash
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+
+# Set this to your base location.
+declare -r service_dir=/usr/local/mesos
+declare -r source_dir="$service_dir/src"
+declare -r build_dir="$source_dir/build"
+declare -r install_dir="$service_dir/install"
+declare -r branch_name="snapshot"
+
+# This is the git revision of ~ 0.12.0 with the state abstraction which we use.
+declare -r git_revision=3758114ee4492dcbb784d5aac65d43ac54ddb439
+
+function all {
+  echo "Updating mesos source..."
+  if [ -d "$source_dir" ]; then
+    pushd $source_dir
+    local revision=`git rev-parse HEAD`
+    git reset --hard
+    if [ $revision != `git rev-parse HEAD` ]; then
+      build
+    fi
+    popd
+  else
+    git clone git://github.com/apache/mesos.git $source_dir
+    (cd $source_dir
+      git checkout $git_revision -b $branch_name
+    )
+    build
+  fi
+}
+
+function build {
+  echo "Building mesos source..."
+  mkdir $build_dir
+  pushd $source_dir
+  ./bootstrap
+  pushd $build_dir
+  "$source_dir/configure" --prefix="$install_dir" --with-curl --disable-perftools
+  make -j 16 check TESTS=
+  ./bin/mesos-tests.sh --gtest_filter=-Cgroups*:FsTest*:Proc*
+  make install
+  popd
+  popd
+}
+
+echo "Installing mesos as user \"$(whoami)\""
+
+mkdir -p "$service_dir"
+all
---
+++ bin/start-chronos.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# This is a sample script for starting chronos. If you deploy the service you may want to build custom
+# runit, monit or upstart scripts.
+CHRONOS_HOME=/Users/florian/airbnb_code/chronos_public/bin/../
+
+# This setup assumes you started with the mesos source and installed the binaries into
+# the ./build directory of the mesos source. Modify this as needed.
+
+# TODO(FL): ensure this script runs on *nix as well.
+# TODO(FL): clean-up!
+echo "This script is setup to run on MacOSX right now. Modify it to run on other systems."
+MESOS_HOME=/usr/local/mesos
+echo "MESOS_HOME is set to: $MESOS_HOME"
+pushd $MESOS_HOME
+libmesos_file=$(find . -name "libmesos.dylib" | head -n1)
+build_env=$(find . -name "mesos-build-env.sh" | head -n1)
+export MESOS_NATIVE_LIBRARY="${MESOS_HOME}/${libmesos_file}"
+echo "MESOS_NATIVE_LIBRARY set to $MESOS_NATIVE_LIBRARY"
+echo "Sourcing mesos-build-env.sh: $build_env"
+source $build_env
+popd
+
+# Start chronos.
+java -cp "$CHRONOS_HOME"/target/chronos*.jar com.airbnb.scheduler.Main server "$CHRONOS_HOME"/config/local_scheduler_nozk.yml
