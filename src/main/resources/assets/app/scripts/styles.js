2c4200275a52f3a5436de0bf994b43aa6f8cbbd8
Andy Kramolisch <a.r.kramolisch@gmail.com>
2013-03-20 18:22:00 -0700

    [web-ui] CHANGELOG below
    CHANGELOG
    =========
    * Removed old dependency on rickshaw.
    * Updated `JobDetailStatsView` to correctly show median value.

--- src/main/resources/assets/app/scripts/styles.js
+++ src/main/resources/assets/app/scripts/styles.js
@@ -2,7 +2,6 @@ define([
   'css!styles/lightbox.css',
   'css!styles/timeline.css',
   'css!styles/pickadate.classic.css',
-  'css!styles/vendor/rickshaw/rickshaw.css',
   'less!styles/select2.less',
   'less!styles/chronos.less'
 ], function() {
