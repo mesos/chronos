662ebf8d1cad0b3358be7b68981e7810c8638a59
Jason Dusek <jason.dusek@gmail.com>
2013-04-10 22:20:30 +0000

    Not tested or anything.

---
+++ bin/ubuntu-one-step.bash
@@ -0,0 +1,114 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+function -h {
+cat <<USAGE
+ USAGE: ...
+
+  Installs Chronos in one step.
+
+USAGE
+}; function --help { -h ;}
+
+prefix=/usr/local
+
+function main {
+  ubuntu
+}
+
+ubuntu_debs=( autoconf make gcc g++ cpp patch python-dev libtool
+              default-jdk default-jdk-builddep default-jre gzip
+              libghc-zlib-dev libcurl4-openssl-dev )
+function ubuntu {
+  debs "${ubuntu_debs[@]}"
+  mesos
+  chronos
+}
+
+mesos_ref=0.12.x
+function mesos {(
+  tmp
+  github_tgz apache/mesos "$mesos_ref" | tgz_into mesos
+  cd mesos
+  build_wrapper mesos_build
+)}
+
+function mesos_build {
+  ./bootstrap
+  ./configure --prefix="$prefix" \
+    --with-webui --with-included-zookeeper --disable-perftools
+  make || make # Always fails the first time.
+  make install
+}
+
+chronos_ref=master
+function chronos {(
+  tmp
+  github_tgz airbnb/chronos "$chronos_ref" | tgz_into chronos
+  cd chronos
+  build_wrapper chronos_build
+)}
+
+function chronos_build {
+  export MESOS_NATIVE_LIBRARY="$prefix"/lib/libmesos.so
+  mvn package
+}
+
+tmp=/tmp/chronos-build."$(hasher "$prefix" "$chronos_ref" "$mesos_ref")" 
+function tmp {
+  [[ ! -d "$tmp" ]] || msg "Recycling build in $tmp"
+  mkdir -p "$tmp"
+  cd "$tmp"
+}
+
+function debs {
+  DEBIAN_FRONTEND=noninteractive apt-get install --yes "$@"
+}
+
+function github_tgz {
+  local repo="$1" ; shift
+  local ref="$1" ; shift
+  curl -fL https://api.github.com/repos/"$repo"/tarball/"$branch" "$@"
+}
+
+function tgz_to {
+  mkdir -p "$1"
+  tar -xz -C "$1" --strip-components 1 # Yes, this is portable.
+}
+
+declare hasher
+function hasher {
+  if [[ ! ${hasher:+isset} ]]
+  then
+    hasher="$(which md5sum)" ||
+    hasher="$(which md5)" ||
+    err "No hashing program found!"
+  fi
+  "$hasher" <<<"$*" | cut -d' ' -f1
+}
+
+function build_wrapper {
+  local dir="$(pwd -P)"
+  local t0="$(date +%T)"
+  msg "$1 $t0 Building..."
+  if "$1" 1>build.out 2>build.err
+  then msg "$1 $t0/$(date +%T)"
+  else
+    local x=$?
+    msg "==== tail -n20 $dir/build.out"
+    tail -n20 "$dir"/build.out >&2
+    msg "==== tail -n20 $dir/build.err"
+    tail -n20 "$dir"/build.err >&2
+    msg "$1 $t0/$(date +%T) failed in $dir."
+    return $(( $x == 0 ? 1 : $x ))
+  fi
+}
+
+function msg { out "$*" >&2 ;}
+function err { local x=$? ; msg "$*" ; return $(( $x == 0 ? 1 : $x )) ;}
+function out { printf '%s\n' "$*" ;}
+
+if [[ ${1:-} ]] && declare -F | cut -d' ' -f3 | fgrep -qx -- "${1:-}"
+then "$@"
+else main "$@"
+fi
+
