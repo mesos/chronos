b730260c99b39cb1b65f640d192d99b2419d4221
Matt Redmond <matt.redmond@airbnb.com>
2013-08-14 10:36:29 -0700

    Split on white space, trim all strings at once.

--- src/main/scala/com/airbnb/scheduler/jobs/JobScheduler.scala
+++ src/main/scala/com/airbnb/scheduler/jobs/JobScheduler.scala
@@ -71,10 +71,10 @@ class JobScheduler @Inject()(val scheduleHorizon: Period,
 
   def sendNotification(job: BaseJob, subject: String, message: Option[String] = None) {
     if (!mailClient.isEmpty) {
-      val subowners = job.owner.split(",")
+      val subowners = job.owner.split("\\s*,\\s*").map(_.trim)
       for (subowner <- subowners) {
-        log.info("Sending mail notification to:%s for job %s".format(subowner.trim(), job.name))
-        mailClient.get !(subowner.trim(), subject, message)
+        log.info("Sending mail notification to:%s for job %s".format(subowner, job.name))
+        mailClient.get !(subowner), subject, message)
       }
     }
 
