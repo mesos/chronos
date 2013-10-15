81dbca59cf5402d174e9c7f984a76a80d7b4dc2f
Tobi Knaup <tobi@knaup.me>
2013-10-15 16:51:41 -0700

    Fix JobMetricsModule

--- src/main/scala/com/airbnb/scheduler/config/JobMetricsModule.scala
+++ src/main/scala/com/airbnb/scheduler/config/JobMetricsModule.scala
@@ -1,11 +1,8 @@
 package com.airbnb.scheduler.config
 
 import com.google.inject.{Scopes, AbstractModule}
-import com.yammer.dropwizard.lifecycle.Managed
-import com.yammer.metrics.core.MetricsRegistry
-import com.yammer.metrics.reporting.GangliaReporter
-import java.util.concurrent.TimeUnit
 import com.airbnb.scheduler.jobs.{MetricReporterService, JobMetrics}
+import com.codahale.metrics.MetricRegistry
 
 /**
  * Author: @andykram
@@ -15,10 +12,10 @@ import com.airbnb.scheduler.jobs.{MetricReporterService, JobMetrics}
 
 class JobMetricsModule (config: SchedulerConfiguration) extends AbstractModule {
 
-  private[this] val registry: MetricsRegistry = new MetricsRegistry()
+  private[this] val registry: MetricRegistry = new MetricRegistry()
 
   def configure() {
-    bind(classOf[MetricsRegistry]).toInstance(registry)
+    bind(classOf[MetricRegistry]).toInstance(registry)
     bind(classOf[MetricReporterService]).toInstance(new MetricReporterService(config, registry))
     bind(classOf[JobMetrics]).in(Scopes.SINGLETON)
   }
