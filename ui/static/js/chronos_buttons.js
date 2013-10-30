830cd8b1d82fa63480e88547cc97553035810901
Matt Redmond <matt.redmond@airbnb.com>
2013-10-29 17:09:30 -0700

    WIP - working on ajax requests to endpoints

--- ui/static/js/chronos_buttons.js
+++ ui/static/js/chronos_buttons.js
@@ -1,14 +1,59 @@
 $(function() {
   $('[id^="run."]').click( function() {
-    alert('Running job ' + $(this).attr("id").substring(4));
+    var name = $(this).attr("id").substring(4);
+    alert('Running job ' + name);
+    $.ajax({
+      //url: {{chronos_host}} + '/scheduler/job/' + name,
+      url: 'http://localhost:4400/scheduler/job/' + name,
+      type: 'PUT',
+      success: function(result) {
+        alert('Ran that job');
+        alert(result);
+      }
+    });
+
   });
 
   $('[id^="edit."]').click( function() {
-    alert('Editing job ' + $(this).attr("id").substring(5));
+    var name = $(this).attr("id").substring(5);
+    alert('Editing job ' + name);
   });
 
-  $('[id^="delete."]').click( function() {
-    alert('Are you ONE HUNDRED percent sure you want to delete ' + $(this).attr("id").substring(7) + '?');
+  $('[id^="kill."]').click( function() {
+    var name = $(this).attr("id").substring(5);
+    alert('Killing tasks for job ' + name);
+    request = $.ajax({
+      //url: {{chronos_host}} + '/scheduler/job/' + name,
+      url: 'http://localhost:4400/scheduler/task/kill' + name,
+      type: 'DELETE'
+    });
+    request.done(function (response, textStatus, jqXHR) {
+      console.log("It seemed to work");
+    });
+    request.fail(function (jqXHR, textStatus, errorThrown) {
+      console.error("The following error occured: " + textStatus, errorThrown);
+    });
+    event.preventDefault();
+  });
+
+
+  $('#delete_job').click( function() {
+    var name = prompt('Which job name do you want to delete?');
+    var result = confirm('Are you sure you want to delete `' + name + '`?');
+    if (result) {
+      request = $.ajax({
+        //url: {{chronos_host}} + '/scheduler/job/' + name,
+        url: 'http://localhost:4400/scheduler/job/' + name,
+        type: 'DELETE'
+      });
+      console.log(request);
+      request.done(function (response, textStatus, jqXHR) {
+        console.log("It seemed to work");
+      });
+      request.fail(function (jqXHR, textStatus, errorThrown) {
+        console.error("The following error occured: " + textStatus, errorThrown);
+      });
+    }
   });
 
   $('#new_job').click( function() {
