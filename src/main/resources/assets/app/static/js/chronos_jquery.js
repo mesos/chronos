157b0e115ad9744afbb74a5cc6387e3f5c008996
Matt Redmond <matt.redmond@airbnb.com>
2013-11-18 11:11:37 -0800

    WIP to get better table layout

--- src/main/resources/assets/app/static/js/chronos_jquery.js
+++ src/main/resources/assets/app/static/js/chronos_jquery.js
@@ -336,11 +336,11 @@ $(function() {
     odd        : ''  // even row zebra striping
   });
   // call the tablesorter plugin and apply the uitheme widget
-  $("table").tablesorter({
+  $("#jobtable").tablesorter({
     // this will apply the bootstrap theme if "uitheme" widget is included
     // the widgetOptions.uitheme is no longer required to be set
     theme : "bootstrap",
-
+    widthFixed : true,
     headerTemplate : '{content} {icon}', // new in v2.7. Needed to add the bootstrap icon!
     // widget code contained in the jquery.tablesorter.widgets.js file
     // use the zebra stripe widget if you plan on hiding any rows (filter widget)
@@ -349,8 +349,7 @@ $(function() {
       // using the default zebra striping class name, so it actually isn't included in the theme variable above
       // this is ONLY needed for bootstrap theming if you are using the filter widget, because rows are hidden
       zebra : ["even", "odd"],
-      // reset filters button
-      filter_reset : ".reset"
+      resizable_addLastColumn : true
     }
   });
 
