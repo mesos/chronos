2cd6fedaa179c3095068690c20299bdafdf3869a
Matt Redmond <matt.redmond@airbnb.com>
2013-11-18 14:08:56 -0800

    WIP

--- src/main/resources/assets/app/static/js/chronos_jquery.js
+++ src/main/resources/assets/app/static/js/chronos_jquery.js
@@ -338,16 +338,14 @@ $(function() {
     // this will apply the bootstrap theme if "uitheme" widget is included
     // the widgetOptions.uitheme is no longer required to be set
     theme : "bootstrap",
-    widthFixed : true,
     headerTemplate : '{content} {icon}', // new in v2.7. Needed to add the bootstrap icon!
     // widget code contained in the jquery.tablesorter.widgets.js file
     // use the zebra stripe widget if you plan on hiding any rows (filter widget)
-    widgets : [ "uitheme", "filter", "zebra"],
+    widgets : [ "uitheme", "resizable", "filter", "zebra"],
     widgetOptions : {
       // using the default zebra striping class name, so it actually isn't included in the theme variable above
       // this is ONLY needed for bootstrap theming if you are using the filter widget, because rows are hidden
       zebra : ["even", "odd"],
-      resizable_addLastColumn : true
     }
   });
 
