a4ea8c46dbe3b9ef493cfb2010179e2a6a0259b8
Matt Redmond <matt.redmond@airbnb.com>
2014-01-16 17:03:04 -0800

    Get the UI back, capn

--- src/main/resources/assets/app/scripts/templates/helpers/bool_disabled.js
+++
@@ -1,7 +0,0 @@
-define(['handlebars'], function (Handlebars) {
-  function boolDisabled(context) {
-    return !!context ? 'Disabled' : 'Enabled';
-  }
-  Handlebars.registerHelper('boolDisabled', boolDisabled);
-  return boolDisabled;
-});
