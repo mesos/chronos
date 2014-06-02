315e26e469c09b5d98fa9ed315d5c28a663b6955
Sebastian Nadorp <sebastian.nadorp@nugg.ad>
2014-06-02 17:45:56 +0200

    Fix http port 80 issue.

--- bin/chronos-sync.rb
+++ bin/chronos-sync.rb
@@ -292,7 +292,7 @@ jobs_to_be_updated.each do |j|
   puts "Sending PUT for `#{job['name']}` to #{uri.request_uri}"
 
   begin
-    res = Net::HTTP.start(uri.hostname, port: uri.port, use_ssl: (uri.port == 443)) do |http|
+    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
       http.request(req)
     end
 
