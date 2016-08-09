5d975be196b1a8e5303c7e9f2b205cf58edafe3c
Stéphane Cottin <stephane.cottin@vixns.com>
2016-08-09 15:27:25 +0200

    fix forcepullimage for mesos containers

--- src/main/scala/org/apache/mesos/chronos/scheduler/mesos/MesosTaskBuilder.scala
+++ src/main/scala/org/apache/mesos/chronos/scheduler/mesos/MesosTaskBuilder.scala
@@ -206,7 +206,7 @@ class MesosTaskBuilder @Inject()(val conf: SchedulerConfiguration) {
               .setName(job.container.image)
               // TODO add setCredential
               .build())
-            .setCached(job.container.forcePullImage)
+            .setCached(!job.container.forcePullImage)
             .build())
           .build())
     }
