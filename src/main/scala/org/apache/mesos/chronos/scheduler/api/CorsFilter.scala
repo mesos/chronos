de789fa5dca000d12d1eed62cc53a1e4fd691163
Brenden Matthews <brenden@diddyinc.com>
2016-12-02 09:15:11 -0500

    Fix build error, style.

--- src/main/scala/org/apache/mesos/chronos/scheduler/api/CorsFilter.scala
+++ src/main/scala/org/apache/mesos/chronos/scheduler/api/CorsFilter.scala
@@ -1,21 +1,23 @@
 package org.apache.mesos.chronos.scheduler.api
 
-import java.util.logging.{Level, Logger}
+import java.util.logging.Logger
 import javax.servlet._
-import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+import javax.servlet.http.HttpServletResponse
 
 class CorsFilter extends Filter {
-  val log = Logger.getLogger(getClass.getName)
+  private val log: Logger = Logger.getLogger(getClass.getName)
 
   def init(filterConfig: FilterConfig) {}
+
   def doFilter(rawRequest: ServletRequest, rawResponse: ServletResponse, chain: FilterChain) {
     rawResponse match {
       case response: HttpServletResponse =>
-          log.debug("Adding cors header to api response")
-          response.setHeader("Access-Control-Allow-Origin", "*");
-          chain.doFilter(rawRequest, rawResponse)
+        log.fine("Adding cors header to api response")
+        response.setHeader("Access-Control-Allow-Origin", "*")
+        chain.doFilter(rawRequest, rawResponse)
       case _ =>
     }
   }
+
   def destroy() {}
 }
