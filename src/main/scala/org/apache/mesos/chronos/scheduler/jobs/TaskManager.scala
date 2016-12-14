597dfec2adb415d14b968a03c5fb37b761940e11
Brenden Matthews <brenden@diddyinc.com>
2016-12-14 12:16:39 -0500

    Don't double enqueue tasks. (#762)
    A task which was already queued should not be re-enqueued.
    
    This is to address an issue not fully address by #750.

--- src/main/scala/org/apache/mesos/chronos/scheduler/jobs/TaskManager.scala
+++ src/main/scala/org/apache/mesos/chronos/scheduler/jobs/TaskManager.scala
@@ -1,5 +1,6 @@
 package org.apache.mesos.chronos.scheduler.jobs
 
+import java.util.Collections
 import java.util.concurrent.{ConcurrentHashMap, Future}
 import java.util.logging.Logger
 
@@ -9,27 +10,29 @@ import com.google.inject.Inject
 import org.apache.mesos.Protos.{TaskID, TaskStatus}
 import org.apache.mesos.chronos.scheduler.config.SchedulerConfiguration
 import org.apache.mesos.chronos.scheduler.graph.JobGraph
-import org.apache.mesos.chronos.scheduler.mesos.{MesosDriverFactory, MesosOfferReviver}
+import org.apache.mesos.chronos.scheduler.mesos.{
+  MesosDriverFactory,
+  MesosOfferReviver
+}
 import org.apache.mesos.chronos.scheduler.state.PersistenceStore
-import org.joda.time.{DateTime, DateTimeZone}
 
 import scala.collection.convert.decorateAsScala._
 import scala.collection.{mutable, _}
-
 
 /**
   * Helps manage task state and the queue which is a buffer where tasks are held until offers come in via chronos.
   *
   * @author Florian Leibert (flo@leibert.de)
   */
-class TaskManager @Inject()(val listeningExecutor: ListeningScheduledExecutorService,
-                            val persistenceStore: PersistenceStore,
-                            val jobGraph: JobGraph,
-                            val mesosDriver: MesosDriverFactory,
-                            val jobsObserver: JobsObserver.Observer,
-                            val registry: MetricRegistry,
-                            val config: SchedulerConfiguration,
-                            val mesosOfferReviver: MesosOfferReviver) {
+class TaskManager @Inject()(
+    val listeningExecutor: ListeningScheduledExecutorService,
+    val persistenceStore: PersistenceStore,
+    val jobGraph: JobGraph,
+    val mesosDriver: MesosDriverFactory,
+    val jobsObserver: JobsObserver.Observer,
+    val registry: MetricRegistry,
+    val config: SchedulerConfiguration,
+    val mesosOfferReviver: MesosOfferReviver) {
 
   val log = Logger.getLogger(getClass.getName)
 
@@ -42,9 +45,14 @@ class TaskManager @Inject()(val listeningExecutor: ListeningScheduledExecutorSer
   val queues = Array[java.util.concurrent.LinkedBlockingQueue[String]](
     new java.util.concurrent.LinkedBlockingQueue[String], // high priority
     new java.util.concurrent.LinkedBlockingQueue[String])
+  val queuedJobs =
+    Collections
+      .newSetFromMap(new ConcurrentHashMap[String, java.lang.Boolean]())
+      .asScala
   // normal
   val names = Array[String]("High priority", "Normal priority")
-  val taskMapping: concurrent.Map[String, mutable.ListBuffer[(String, Future[_])]] =
+  val taskMapping: concurrent.Map[String,
+                                  mutable.ListBuffer[(String, Future[_])]] =
     new ConcurrentHashMap[String, mutable.ListBuffer[(String, Future[_])]]().asScala
   val queueGauge = registry.register(
     MetricRegistry.name(classOf[TaskManager], "queueSize"),
@@ -84,15 +92,16 @@ class TaskManager @Inject()(val listeningExecutor: ListeningScheduledExecutorSer
     try {
       val (jobName, _, _, _) = TaskUtils.parseTaskId(taskId)
       if (runningTasks.contains(jobName)) {
-        val remainingTasks = runningTasks(jobName)
-          .filter(task => task.taskId != taskId)
+        val remainingTasks =
+          runningTasks(jobName).filter(task => task.taskId != taskId)
         runningTasks(jobName) = remainingTasks
         if (runningTasks(jobName).isEmpty) {
           runningTasks.remove(jobName)
         }
       }
     } catch {
-      case _: scala.MatchError => log.warning(s"Can't remove unknown task $taskId")
+      case _: scala.MatchError =>
+        log.warning(s"Can't remove unknown task $taskId")
     }
   }
 
@@ -104,19 +113,15 @@ class TaskManager @Inject()(val listeningExecutor: ListeningScheduledExecutorSer
       val tasks = runningTasks(jobName)
       runningTasks(jobName) = tasks.map(task => {
         if (task.taskId == taskId) {
-          new ChronosTask(slaveId,
-            taskId,
-            Some(taskStatus))
+          new ChronosTask(slaveId, taskId, Some(taskStatus))
         } else {
           task
         }
       })
     } else {
       log.warning(s"Received status update for untracked jobName=$jobName")
-      runningTasks(jobName) =
-        List(new ChronosTask(slaveId,
-          taskId,
-          Some(taskStatus)))
+      runningTasks(jobName) = List(
+        new ChronosTask(slaveId, taskId, Some(taskStatus)))
     }
   }
 
@@ -124,9 +129,8 @@ class TaskManager @Inject()(val listeningExecutor: ListeningScheduledExecutorSer
     // Remove any running jobs from this slave
     val jobs = runningTasks.map {
       case (jobName, tasks) =>
-        val remaining = tasks.filter {
-          task =>
-            slaveId == task.slaveId
+        val remaining = tasks.filter { task =>
+          slaveId == task.slaveId
         }
         (jobName, remaining)
     }
@@ -157,8 +161,10 @@ class TaskManager @Inject()(val listeningExecutor: ListeningScheduledExecutorSer
       log.fine(s"$name queue empty")
       None
     } else {
+      removedFromQueue(taskId)
       log.info(s"$name queue contains task: $taskId")
-      val jobOption = jobGraph.getJobForName(TaskUtils.getJobNameForTaskId(taskId))
+      val jobOption =
+        jobGraph.getJobForName(TaskUtils.getJobNameForTaskId(taskId))
 
       //If the job was deleted after the taskId was added to the queue, the task could be empty.
       if (jobOption.isEmpty) {
@@ -180,16 +186,6 @@ class TaskManager @Inject()(val listeningExecutor: ListeningScheduledExecutorSer
   }
 
   /**
-    * Returns the time that is left before the task needs to be handed off to chronos where it is immediately executed.
-    *
-    * @param due DateTime when the job should be run
-    * @return the number of milliseconds between current time and when the task is due
-    */
-  def getMillisUntilExecution(due: DateTime) = {
-    scala.math.max(0L, due.getMillis - new DateTime(DateTimeZone.UTC).getMillis)
-  }
-
-  /**
     * Removes a future-task mapping thus signaling that a task has been added to the local queue awaiting execution from
     * chronos.
     *
@@ -199,8 +195,8 @@ class TaskManager @Inject()(val listeningExecutor: ListeningScheduledExecutorSer
     log.info("Removing task mapping")
     taskMapping.get(task.job.name) match {
       case Some(i) =>
-        taskMapping += (task.job.name -> i.filter({
-          x => x._1 != task.taskId
+        taskMapping += (task.job.name -> i.filter({ x =>
+          x._1 != task.taskId
         }))
       case _ =>
     }
@@ -210,24 +206,48 @@ class TaskManager @Inject()(val listeningExecutor: ListeningScheduledExecutorSer
     * Cancels all tasks that are delay scheduled with the underlying executor.
     */
   def flush() {
-    taskMapping.clone().values.foreach(
-      _.foreach {
-        case (taskId, futureTask) =>
-          log.info("Cancelling task '%s'".format(taskId))
-          futureTask.cancel(true)
-      }
-    )
+    taskMapping
+      .clone()
+      .values
+      .foreach(
+        _.foreach {
+          case (taskId, futureTask) =>
+            log.info("Cancelling task '%s'".format(taskId))
+            futureTask.cancel(true)
+        }
+      )
     taskMapping.clear()
     queues.foreach(_.clear())
   }
 
+  def queueContains(taskId: String): Boolean = {
+    val jobName = TaskUtils.getJobNameForTaskId(taskId)
+    queuedJobs.contains(jobName)
+  }
+
+  def setAsQueued(taskId: String): Unit = {
+    val jobName = TaskUtils.getJobNameForTaskId(taskId)
+    queuedJobs += jobName
+  }
+
+  def removedFromQueue(taskId: String): Unit = {
+    val jobName = TaskUtils.getJobNameForTaskId(taskId)
+    queuedJobs -= jobName
+  }
+
   def enqueue(taskId: String, highPriority: Boolean) {
-    /* Don't want to change previous logging if we don't have to... */
-    log.fine(s"Adding task '$taskId' to ${if (highPriority) "high priority" else ""} queue")
-    val _priority = if (highPriority) HIGH_PRIORITY else NORMAL_PRIORITY
-    this.synchronized {
-      queues(_priority).add(taskId)
+    // Check if it's already enqueued
+    if (queueContains(taskId)) {
+      log.fine(
+        s"Task '$taskId' asked to be queued, but it was already enqueued")
+      return
     }
+    /* Don't want to change previous logging if we don't have to... */
+    log.fine(
+      s"Adding task '$taskId' to ${if (highPriority) "high priority" else ""} queue")
+    val _priority = if (highPriority) HIGH_PRIORITY else NORMAL_PRIORITY
+    setAsQueued(taskId)
+    queues(_priority).add(taskId)
 
     val jobName = TaskUtils.getJobNameForTaskId(taskId)
     val jobOption = jobGraph.lookupVertex(jobName)
@@ -248,7 +268,8 @@ class TaskManager @Inject()(val listeningExecutor: ListeningScheduledExecutorSer
     if (runningTasks.contains(job.name)) {
       runningTasks(job.name).foreach({ task =>
         log.warning(s"Killing taskId=${task.taskId})")
-        mesosDriver.get.killTask(TaskID.newBuilder().setValue(task.taskId).build())
+        mesosDriver.get.killTask(
+          TaskID.newBuilder().setValue(task.taskId).build())
       })
     }
   }
